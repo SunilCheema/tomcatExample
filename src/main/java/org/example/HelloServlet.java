6ed42e5565fba133c12a13e5a64b54b446b1950f
sunil cheema <sunilcheema@sunils-MacBook-Pro-2.local>
2018-05-31 14:20:18 +0100

    added letter

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -12,6 +12,6 @@ public class HelloServlet extends HttpServlet
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException
     {
-        resp.getWriter().print("Hello worlds");
+        resp.getWriter().print("Hello worlds 2");
     }
 }
